a787bee29034229530038ac94740cd56d202b0a5
daryl galvez <ds@silverblue>
2023-09-27 23:20:11 +0800

    initial commit
    script still not working

---
+++ password-cracker.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+from pwn import *
+import sys
+
+if len(sys.argv) != 2:
+    print("Invalid Arguments!")
+    print(">> {} <sha256sum>".format(sys.argv[0]))
+    exit()
+
+wanted_hash = sys.argv[1]
+password_file = "rockyou.txt"
+attempts = 0
+
+with log.progress("Attempting to back: {}!\n".format(wanted_hash)) as p:
+    with open(password_file, "r", encoding='latin-1') as password_list:
+        for password in password_list:
+            password = password.strip("\n").encode('latin-1')
+            password_hash = sha256sumhex(password)
+            p.status("[{}] {} == {}".format(attempts, password.decode('latin-1'),password))
+            if password_hash == wanted_hash:
+                p.success("Password hash found after {} attempts! {} hashes to {}!".format(attempts, password, password_hash))
+                exit()
+            attempts += 1
+            p.failure("Password hash not found!")
